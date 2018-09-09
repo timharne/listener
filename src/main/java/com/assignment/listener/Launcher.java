bb6513592bea8d84fd6ca1b1a9a070c9ec658c73
Timothy Harne <tim@harne.net>
2018-09-09 16:24:03 -0400

    Finish requirements

--- src/main/java/com/assignment/listener/Launcher.java
+++ src/main/java/com/assignment/listener/Launcher.java
@@ -3,10 +3,18 @@ package com.assignment.listener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The type Launcher.
+ */
 @SpringBootApplication
 public class Launcher {
 
-	public static void main(String[] args) {
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
 		SpringApplication.run(Launcher.class, args);
 	}
 
