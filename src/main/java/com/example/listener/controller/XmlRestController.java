c6b438f3799693f86994c7064ed2ada574095e7a
Timothy Harne <tim@harne.net>
2018-09-09 14:23:42 -0400

    update post function and modify xml file

---
+++ src/main/java/com/example/listener/controller/XmlRestController.java
@@ -0,0 +1,36 @@
+package com.example.listener.controller;
+
+import com.example.listener.model.Watchlist;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/listener")
+public class XmlRestController {
+
+    @Value("${spring.file}")
+    private String jsonFile;
+
+    @GetMapping("/status")
+	public String homePage(HttpServletResponse response)  {
+		String message = "Service Status: Live!";
+		return message;
+	}
+
+	@PostMapping("/watchlist")
+	public String postWatchlist(HttpServletRequest request, @RequestBody Watchlist watchlist) throws IOException {
+		ObjectMapper mapper = new ObjectMapper();
+        mapper.enable(SerializationFeature.INDENT_OUTPUT);
+        mapper.writeValue(new File(jsonFile), watchlist );
+        return "watchlist";
+	}
+
+}
+
